4dff26588b21571be9afa40d86df7f9f9f5eef70
RomanHrbek <roman.hrbek@seznam.cz>
2023-09-29 00:44:10 +0200

    project completed

---
+++ index.js
@@ -0,0 +1,36 @@
+let countHome = document.getElementById("countHome")
+let count = 0
+
+
+function increment1() {
+    count += 1
+    countHome.textContent = count
+}
+
+function increment2() {
+    count += 2
+    countHome.textContent = count
+}
+
+function increment3() {
+    count += 3
+    countHome.textContent = count
+}
+
+let countGuest = document.getElementById("countGuest")
+let countG = 0
+
+function incrementguest1() {
+    countG += 1
+    countGuest.textContent = countG
+}
+
+function incrementguest2() {
+    countG += 2
+    countGuest.textContent = countG
+}
+
+function incrementguest3() {
+    countG += 3
+    countGuest.textContent = countG
+}
